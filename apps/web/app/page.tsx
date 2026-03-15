94f866de6f0e94e918bb218c46d31c13e6712cd3
adityavn03 <aditya.vn03@gmail.com>
2026-03-15 15:26:12 +0000

    after clearing the error

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -18,6 +18,9 @@ export default async function Home() {
       <div>
         <h1> Check condition for the staging area </h1>
       </div>
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem fugiat illum quam commodi modi accusantium, necessitatibus magnam non exercitationem recusandae cum nisi? Sint amet eum nobis quia molestias, eveniet ab quam aspernatur sed modi fugiat sequi porro qui, minima perferendis ipsa atque laboriosam dolore harum cumque voluptates. Saepe, suscipit ab.</p>
+      </div>
       <h1>Hello from BMS Web App 🚀</h1>
 
       <h2>Users in Database</h2>
